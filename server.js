6b4240901254c407e5da977c9c897a69a373499b
Daniel Shoup <79939129+danshoup@users.noreply.github.com>
2021-06-09 21:13:46 -0400

    routes updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //Dependencies
 
+const { table } = require('console');
 const express = require('express');
 const path = require('path');
 
@@ -8,9 +9,44 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const tables = [];
+const waitlist = [];
+
+// Routes:
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
+
+app.get('/api/tables', (req, res) => res.json(tables));
+
+app.post('/api/reserve', (req, res) => {
+    const newReserve = req.body;
+
+    newReserve.routeName = newReserve.name;
+  
+    if (tables.length < 5) {
+        tables.push(newReserve);
+        res.send(true);
+    } else {
+        waitlist.push(newReserve)
+        res.send(false);
+    }
+    
+  });
+  
+
+
+
 const handleRequest = (req, res) => {
     
 };
+
 
 
 
